c4aed3b862f1b79aff9ea2c7a137efca8188b435
preston <broberts9@gmail.com>
2017-05-18 21:17:14 -0700

    initial push; wireframe ready

---
+++ app.js
@@ -0,0 +1,12 @@
+var token = "CAUQAA";
+var q = "dogs";
+$.ajax(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBZw_Dg7LohwJhi_O7ZOOz--qFthIyVlFM&part=snippet&${q}&pageToken=${token}`, {
+    success: function (data) {
+        console.log(data);
+        var items = data.items.map(function (video) {
+            console.log(video);
+            return `<a href="">${video.snippet.channelTitle}</a>`
+        })
+        $('.display-answer').html(items);
+    }
+})
